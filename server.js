86d63df40f3b3680f4932bcc11f590b5de88ff8f
Gabriel <gabriel.almeida.12.biel@gmail.com>
2022-06-22 14:24:00 +0000

    Adicionando divisão

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get('/multiplicacao/:n1/:n2', (req, res) => {
     res.status(200).json({ resultado: n1*n2 })
 })
 
+// divisão
+app.get('/divisao/:n1/:n2', (req, res) => {
+    const n1 = +req.params.n1
+    const n2 = +req.params.n2
+
+    if(isNaN(n1) || isNaN(n2)){
+        res.status(400).send('Sorry we cannot complete the operation please send a number on the request')
+    }
+
+    res.status(200).json({ resultado: n1/n2 })
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening to the port ${PORT}`);
 })
